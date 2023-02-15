2cc04177d127b671213f009c198648e80ceede61
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-02-15 08:10:19 +0300

    adding viewsets and routers

--- todonotes/todo/serializers.py
+++ todonotes/todo/serializers.py
@@ -3,12 +3,12 @@ from rest_framework.serializers import ModelSerializer
 from todo.models import Project, Todo
 
 
-class ProjectSerializer(ModelSerializer):
+class ProjectModelSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
 
-class TodoSerializer(ModelSerializer):
+class TodoModelSerializer(ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'

5961ed45b212a0afdc7158e28bff7e69570026f8
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-02-17 17:55:02 +0300

    adding filters

--- todonotes/todo/views.py
+++ todonotes/todo/views.py
@@ -8,8 +8,19 @@ from todo.serializers import ProjectModelSerializer, TodoModelSerializer
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    def get_queryset(self):
+        name = self.request.query_params.get('name','')
+        project = Project.objects.all()
+        if name:
+            project = project.filter(name__contains=name)
+        return project
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
-
+    def get_queryset(self):
+        project = self.request.query_params.get('project','')
+        todo = Todo.objects.all()
+        if project:
+            todo = todo.filter(project=project)
+        return todo

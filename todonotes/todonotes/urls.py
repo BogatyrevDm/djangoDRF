ead5ec570b23f4786a6735ea20eebfb017f2f71a
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-02-17 17:17:30 +0300

    correcting viewsets

--- todonotes/todonotes/urls.py
+++ todonotes/todonotes/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from todo.views import ProjectModelViewSet, TodoModelViewSet
-from users.views import UserModelViewSet
+from users.views import UserCustomViewSet
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
+router.register('users', UserCustomViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todos', TodoModelViewSet)
 

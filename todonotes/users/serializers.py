3491293693cb7106d0e6cf4f73068c5023817135
BogatyrevDm <bogatyrevdm82@gmail.com>
2023-03-13 19:22:26 +0300

    Lesson9 (#9)
    * Lesson 9 Documentation
    
    * Lesson 9 versions

--- todonotes/users/serializers.py
+++ todonotes/users/serializers.py
@@ -2,8 +2,14 @@ from rest_framework.serializers import ModelSerializer
 
 from .models import User
 
-class UserModelSerializer(ModelSerializer):
 
+class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = 'username', 'first_name', 'last_name', 'email'
+
+
+class UserModelSerializerFull(ModelSerializer):
+    class Meta:
+        model = User
+        fields = 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff'
